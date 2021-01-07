456c2debafc2fef033f429c08b7db1324531e85d
bormentallp <petro4393@gmail.com>
2021-01-07 17:35:10 +0200

    works

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('.slider').slick({
+    dots: true,
+  });
+  $('.qutes__slider').slick({
+    dots: true,
+    slidesToShow: 1,
+    arrows: false,
+    dragable: false,
+  });
+});
