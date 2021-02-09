45eacc3e8cc02ad056a060229f6923e0870c03bf
bormentallp <petro4393@gmail.com>
2021-02-09 21:22:57 +0200

    portfolio till stars

---
+++ portfolioPage/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $('.header__burger').click(function (event) {
+    $('.header__burger,.header__menu, .leng').toggleClass('active');
+    $('.name,.profecional, .face__img ').toggleClass('schow');
+  });
+});
+const ratingItemsList = document.querySelectorAll('.rating__icons');
+const ratingItemsArray = Array.prototype.slice.call(ratingItemsList);
+
+ratingItemsArray.forEach((item) =>
+  item.addEventListner('click', () => {
+    const { itemValue } = item.dataset;
+    item.parentNode.dataset.totalValue = itemValue;
+  })
+);
